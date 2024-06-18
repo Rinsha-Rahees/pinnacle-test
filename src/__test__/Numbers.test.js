ef08d14a2c1666759d212b52518629132afe6535
RinshaRahees <104898673+RinshaRahees@users.noreply.github.com>
2024-06-18 16:37:06 +0530

    test update according to initial render

--- src/__test__/Numbers.test.js
+++ src/__test__/Numbers.test.js
@@ -8,7 +8,7 @@ test("Should verify ascending and descending", async () => {
     await act(() => render(<Numbers/>))
 
     const display = screen.getByTestId("display")
-    expect(display).toHaveTextContent("15672")
+    expect(display).toHaveTextContent("12567")
 
     const ascBtn = screen.getByText("Ascending")
     const dscBtn = screen.getByText("Descending")
